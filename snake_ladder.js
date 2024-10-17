98dc60bc58cf33899e1c66b6f5fe58335240c671
suhail-z <suhail22rm@gmail.com>
2024-10-17 20:18:58 +0530

    reload-updated

--- snake_ladder.js
+++ snake_ladder.js
@@ -15,62 +15,65 @@ var player=0;
 var properties=['Snake','Ladder','No Play'];
 
 console.log("🐍-----SNAKE & LADDERS-----\n");
+function play(){
 
-input.question('Enter No Of Players :  ',(reply)=>{
+    input.question('Enter No Of Players :  ',(reply)=>{
     
-var total_player=parseInt(reply);
-
-function roll(){
-    let dice=Math.floor(Math.random()*6+1);
-    let turn=Math.floor(Math.random()*3);
-    let current=player%total_player;
-    players[current].rolls++;
-
-    if(turn!=2){
-    if(players[current].status===0 && dice===1 && turn!=0){
-        players[current].status=1;
-        console.log(`player ${current+1} have started !`);
+        var total_player=parseInt(reply);
         
-    }
-     if(players[current].status===1){
+        function roll(){
+            let dice=Math.floor(Math.random()*6+1);
+            let turn=Math.floor(Math.random()*3);
+            let current=player%total_player;
+            players[current].rolls++;
         
-
-        if(turn==0 && players[current].score<=dice){
-            players[current].score=0;
+            if(turn!=2){
+            if(players[current].status===0 && dice===1 && turn!=0){
+                players[current].status=1;
+                console.log(`player ${current+1} have started !`);
+                
+            }
+             if(players[current].status===1){
+                
+        
+                if(turn==0 && players[current].score<=dice){
+                    players[current].score=0;
+                }
+                else if(turn ==0){
+                    players[current].score-=dice;
+                }
+                else if(turn==1 && players[current].score+dice<=100){
+                    players[current].score+=dice;
+                }
+            }
         }
-        else if(turn ==0){
-            players[current].score-=dice;
+                console.log(`PLAYER ${current+1}\n dice :${dice}\n score :${players[current].score}\n Rolls :${players[current].rolls}\n Turns :${properties[turn]}\n`);
+            
+            if(players[current].score==100){
+               console.log(`\n PLayer ${current+1} Wins !!!`);
+            }
+            else if(dice==6 || turn==1){
+                roll();
+            }
+            else{
+                player++;
+                roll();
+            }
+        
         }
-        else if(turn==1 && players[current].score+dice<=100){
-            players[current].score+=dice;
+        
+        if(total_player>1){
+        
+            for(i=0;i<total_player;i++){
+            players.push(details());
+            } roll();
         }
-    }
+        else{
+            console.log('Enter Valid Players !');
+            play();
+        }
+        
+        
+         });
 }
-        console.log(`PLAYER ${current+1}\n dice :${dice}\n score :${players[current].score}\n Rolls :${players[current].rolls}\n Turns :${properties[turn]}\n`);
-    
-    if(players[current].score==100){
-       console.log(`\n PLayer ${current+1} Wins !!!`);
-    }
-    else if(dice==6 || turn==1){
-        roll();
-    }
-    else{
-        player++;
-        roll();
-    }
-
-}
-
-if(total_player>1){
-
-    for(i=0;i<total_player;i++){
-    players.push(details());
-    } roll();
-}
-else{
-    console.log('Enter Valid Players !');
-    
-}
-
-
- });
+play();
